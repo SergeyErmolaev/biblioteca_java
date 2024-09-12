4c2fcda9d5207b52ab236e5e65fcd27b6538c7a7
Sergey Ermolaev <ro1sergey.se@gmail.com>
2024-09-12 19:15:52 +0200

    comments class

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,22 +1,47 @@
 public class Biblioteca {
     public static void main(String[] args) {
-        char au_id;
-        char pub_id;
-        char title_id;
 
-        String au_fname;
-        String au_lname;
-        String title_name;
-        String pub_name;
-        String city;
-        String type;
+        /*
+		 * We need to have variables that identify the reader's book, the library system administrator, and the librarian.
+		 * Let's start with the variables that identify the book.
+		*/
 
-        int pages;
-        int day;
-        int month;
-        int year;
-        int au_order;
+        private char au_id;
+        private char pub_id;
+        private char title_id;
+
+        private String au_fname;
+        private String au_lname;
+        private String title_name;
+        private String pub_name;
+        private String city;
+        private String type;
+
+        private int pages;
+        private int day;
+        private int month;
+        private int year;
+        private int au_order;
         
+        /*
+         * Also needed are methods to connect to the database with different permissions.
+         * Administrator
+         * add/edit/delete users with different permissions (administrator, librarian, reader).
+         * add/edit/delete books
+         * See all members.
+         * See all books
+         * 
+         * Librarian
+         * add/edit/delete books.
+         * add/edit/delete readers
+         * See all readers/all books
+         * 
+         * Reader
+         * view books
+         * get a book
+         * return a book
+         * change your personal data
+         * */
 
         Biblioteca biblioteca = new Biblioteca();
         biblioteca.open();
